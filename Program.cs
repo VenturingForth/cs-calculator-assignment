7cbac1bdbacd9ebc903e97b279c3816c06fb8e08
VenturingForth <joshua@codeadventure.uk>
2024-06-05 08:45:57 +0100

    Refactored string results and input checks into their own methods.

--- Program.cs
+++ Program.cs
@@ -13,17 +13,20 @@ Console.WriteLine("[S]ubtract");
 Console.WriteLine("[M]ultiply");
 userInput = Console.ReadLine();
 
-if (userInput == "a" || userInput == "A")
+if (EqualsCaseInsensitive(userInput, "A"))
 {
-    Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+    int sum = firstNumber + secondNumber;
+    PrintFinalEquation(firstNumber, secondNumber, sum, "+");
 } 
-else if (userInput == "s" || userInput == "S")
+else if (EqualsCaseInsensitive(userInput, "S"))
 {
-    Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+    int difference = firstNumber - secondNumber;
+    PrintFinalEquation(firstNumber, secondNumber, difference, "-");
 }
-else if (userInput == "m" ||  userInput == "M")
+else if (EqualsCaseInsensitive(userInput, "M"))
 {
-    Console.WriteLine($"{firstNumber} x {secondNumber} = {firstNumber * secondNumber}");
+    int multiplied = firstNumber * secondNumber;
+    PrintFinalEquation(firstNumber, secondNumber, multiplied, "x");
 }
 else
 {
@@ -32,3 +35,15 @@ else
 
 Console.WriteLine("Press any key to close.");
 Console.ReadKey();
+
+void PrintFinalEquation(
+    int number1, int number2, int result, string @operator)
+{
+    Console.WriteLine(
+        $"{number1} {@operator} {number2} = {result}");
+}
+
+bool EqualsCaseInsensitive(string left, string right)
+{
+    return left.ToUpper() == right.ToUpper();
+}
